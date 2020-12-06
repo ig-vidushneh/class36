719d15fc015b999d1875b5890e2a85dff96fe551
ig-vidushneh <73419827+ig-vidushneh@users.noreply.github.com>
2020-12-06 15:47:37 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,28 @@
+class Game{
+    constructor(){}
+    getState(){
+        var gameStateRef=database.ref('gameState');
+        gameStateRef.on("value",function(data){
+            gameState=data.val();
+        })
+    }
+    update(state){
+        database.ref('/').update({
+            gameState:state
+        });
+    }
+    async start(){
+        if(gameState===0){
+            player=new Player();
+            var playerCountref=await database.ref('playerCount').once("value")
+            if(playerCountref.exists())
+            {
+                playerCount=playerCountref.val();
+                player.getCount();
+            }
+            form=new Form()
+            form.display();
+        }
+    }
+}
+
